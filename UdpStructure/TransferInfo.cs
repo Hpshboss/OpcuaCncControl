1459c6875fe1270ccba15fbd31864ecbcee4fb83
Hpshboss <49175517+Hpshboss@users.noreply.github.com>
2019-10-07 20:48:45 +0800

    Create TransferInfo.cs

---
+++ UdpStructure/TransferInfo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdpStructure
+{
+    [Serializable]
+    public class TransferInfo
+    {
+        public ushort ID;
+        public ushort Sz;
+        public byte Cmd;
+        public ushort Count;
+        public byte Sum;
+        public TransferInfo(ushort ID, ushort Sz, byte Cmd, ushort Count, byte Sum)
+        {
+            this.ID = ID;
+            this.Sz = Sz;
+            this.Cmd = Cmd;
+            this.Count = Count;
+            this.Sum = Sum;
+        }
+    }
+}
