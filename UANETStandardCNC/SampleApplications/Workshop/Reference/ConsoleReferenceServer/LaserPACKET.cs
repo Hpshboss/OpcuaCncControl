675e3869c46ad2e1db94a58decf313c94c7e1032
Hpshboss <49175517+Hpshboss@users.noreply.github.com>
2019-11-01 00:58:35 +0800

    雷射機台參數樹狀資料結構
    利用OPCUA開源專案"ConsoleReferenceServer"建構之雷射機台參數樹狀資料結構
    
    開啟方式:
    UANETStandardCNC\SampleApplications\Workshop\Reference\UA Reference.sln

---
+++ UANETStandardCNC/SampleApplications/Workshop/Reference/ConsoleReferenceServer/LaserPACKET.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Quickstarts.ReferenceServer
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    public struct LaserPACKET  //雷射封包結構體
+    {
+        public uint DateTime;   //系統時間
+        public uint TotalWCount;   //累計工作時間
+        public uint ThisWCount;   //目前加工時間
+        public byte Unit;
+        public byte CStart;
+        public byte CStop;
+        public byte MemStart;  //MemStart
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public char[] Fn;   //加工程式名稱 FileName
+        public uint Nn, Bn;
+        public ushort MMode;   //系統模式
+        public ushort MStatus;   //機台狀態
+        public ushort SCode, Ms;   //抓資料的兩個變數
+                                   //------------------------------
+        public LaserCOORD Coord;
+        public LaserSDATA SData;
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    public struct LaserSDATA//<64Byte> //雷射本身加工參數
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public char[] Flag;
+        public byte Mode;      //CW\QCW
+        public byte Gas;       //Air,N2,O2
+        public ushort Power;     //0~10000 unit:0.01%
+        public ushort ResW;     //不理他
+        public uint Dura;     //0~50000 unit:0.001ms 佔空比
+        public uint Hz;       //0~5000000 unit:0.01hz
+        public uint RefHt;    //pulse
+        public uint Dead;     //pulse
+        public ushort AirBar;       //0~50000 unit:0.001bar
+        public ushort Kp;        //0~50000 unit:0.001 
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public char[] Comment;  //comment
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 28)]
+        public char[] ResvB1;
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    public struct LaserCOORD  //各軸參數
+    {
+        public PT9L Mp, Pp, Lp, Dp;                     //座標群
+        public PT9L RefWp, OftWp;
+        public double Speed;                          //移動速度
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public uint[] Rpm;      //各軸轉速
+    }
+    //---------------------------------
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    public struct PT9L
+    {
+        public int x, y, z, a, b, c, u, v, w;
+    }
+}
---
+++ UANETStandardCNC/SampleApplications/Workshop/Reference/ConsoleReferenceServer/Program.cs
@@ -0,0 +1,393 @@
+﻿/* ========================================================================
+ * Copyright (c) 2005-2019 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
+
+using Mono.Options;
+using Opc.Ua;
+using Opc.Ua.Configuration;
+using Opc.Ua.Server;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Quickstarts.ReferenceServer
+{
+    //共用變數
+    public static class Globals
+    {
+        public static int numberThreads = 1; //quantity of server
+        public static byte[] DataBuf = new byte[532];
+        public static LaserPACKET LaserPACK = new LaserPACKET();
+    }
+
+
+    public class ApplicationMessageDlg : IApplicationMessageDlg
+    {
+        private string message = string.Empty;
+        private bool ask = false;
+
+        public override void Message(string text, bool ask)
+        {
+            this.message = text;
+            this.ask = ask;
+        }
+
+        public override async Task<bool> ShowAsync()
+        {
+            if (ask)
+            {
+                message += " (y/n, default y): ";
+                Console.Write(message);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+            if (ask)
+            {
+                try
+                {
+                    ConsoleKeyInfo result = Console.ReadKey();
+                    Console.WriteLine();
+                    return await Task.FromResult((result.KeyChar == 'y') || (result.KeyChar == 'Y') || (result.KeyChar == '\r'));
+                }
+                catch
+                {
+                    // intentionally fall through
+                }
+            }
+            return await Task.FromResult(true);
+        }
+    }
+
+    public enum ExitCode : int
+    {
+        Ok = 0,
+        ErrorServerNotStarted = 0x80,
+        ErrorServerRunning = 0x81,
+        ErrorServerException = 0x82,
+        ErrorInvalidCommandLine = 0x100
+    };
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            #region Open a thread for pipeServer
+            int i;
+            Thread[] servers = new Thread[Globals.numberThreads];
+
+            Console.WriteLine("Waiting for client connect...\n");
+            for (i = 0; i < Globals.numberThreads; i++)
+            {
+                servers[i] = new Thread(PipeServerThread);
+                servers[i].Start();
+            }
+            #endregion
+            Console.WriteLine("{0} OPC UA Reference Server", Utils.IsRunningOnMono() ? "Mono" : ".Net Core");
+            
+            // command line options
+            bool showHelp = false;
+            bool autoAccept = false;
+
+            Mono.Options.OptionSet options = new Mono.Options.OptionSet {
+                { "h|help", "show this message and exit", h => showHelp = h != null },
+                { "a|autoaccept", "auto accept certificates (for testing only)", a => autoAccept = a != null }
+            };
+
+            try
+            {
+                IList<string> extraArgs = options.Parse(args);
+                foreach (string extraArg in extraArgs)
+                {
+                    Console.WriteLine("Error: Unknown option: {0}", extraArg);
+                    showHelp = true;
+                }
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine(e.Message);
+                showHelp = true;
+            }
+
+            if (showHelp)
+            {
+                Console.WriteLine(Utils.IsRunningOnMono() ? "Usage: mono MonoReferenceServer.exe [OPTIONS]" : "Usage: dotnet ConsoleReferenceServer.dll [OPTIONS]");
+                Console.WriteLine();
+
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
+                return (int)ExitCode.ErrorInvalidCommandLine;
+            }
+
+            MyRefServer server = new MyRefServer(autoAccept);
+            server.Run();
+
+            return (int)MyRefServer.ExitCode;
+        }
+
+        #region LaserPackageThread
+        private static void PipeServerThread(object obj)
+        {
+            
+            NamedPipeServerStream pipeServer =
+                new NamedPipeServerStream("LaserPipe", PipeDirection.In, Globals.numberThreads);
+
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            // wait for a client to connect
+            pipeServer.WaitForConnection();
+
+            Console.WriteLine("Client connected on Thread[{0}]", threadId);
+            StreamString ss = new StreamString(pipeServer);
+            while (true)
+            {
+                try
+                {
+                    Globals.DataBuf = ss.ReadBytes();
+                    //then turn the Bytes Array to LaserPACK Struct
+                    Globals.LaserPACK = (LaserPACKET)ByteToStruct(Globals.DataBuf, typeof(LaserPACKET));
+                    //check the data
+                    Console.WriteLine(Globals.LaserPACK.DateTime);
+                    Console.ReadKey();
+                }
+                // Catch the IOException that is raised if the pipe is broken
+                // or disconnected.
+                catch (IOException e)
+                {
+                    Console.WriteLine("ERROR: {0}", e.Message);
+                }
+            }
+            //pipeServer.Close();
+            
+        }
+
+        //把收集到之DataBuf[](位元組陣列)轉換為廠商提供之雷射封包(結構體)形式
+        private static object ByteToStruct(byte[] bytes, Type type)
+        {
+            int size = Marshal.SizeOf(type);
+            Console.WriteLine("LaserPACKET size = " + size);
+            Console.WriteLine("DataBuf size = " + bytes.Length);
+
+            //分配結構體記憶體空間
+            IntPtr structPtr = Marshal.AllocHGlobal(size);
+            //將byte陣列拷貝到分配好的記憶體空間
+            Marshal.Copy(bytes, 0, structPtr, size);
+            try
+            {
+                //將記憶體空間轉換為目標結構體
+                object obj = Marshal.PtrToStructure(structPtr, type);
+                return obj;
+            }
+            finally
+            {
+                //釋放記憶體空間
+                Marshal.FreeHGlobal(structPtr);
+            }
+        }
+        #endregion
+    }
+
+    public class MyRefServer
+    {
+        ReferenceServer server;
+        Task status;
+        DateTime lastEventTime;
+        static bool autoAccept = false;
+        static ExitCode exitCode;
+
+        public MyRefServer(bool _autoAccept)
+        {
+            autoAccept = _autoAccept;
+        }
+
+        public void Run()
+        {
+
+            try
+            {
+                exitCode = ExitCode.ErrorServerNotStarted;
+                ConsoleSampleServer().Wait();
+                Console.WriteLine("Server started. Press Ctrl-C to exit...");
+                exitCode = ExitCode.ErrorServerRunning;
+            }
+            catch (Exception ex)
+            {
+                Utils.Trace("ServiceResultException:" + ex.Message);
+                Console.WriteLine("Exception: {0}", ex.Message);
+                exitCode = ExitCode.ErrorServerException;
+                return;
+            }
+
+            ManualResetEvent quitEvent = new ManualResetEvent(false);
+            try
+            {
+                Console.CancelKeyPress += (sender, eArgs) =>
+                {
+                    quitEvent.Set();
+                    eArgs.Cancel = true;
+                };
+            }
+            catch
+            {
+            }
+
+            // wait for timeout or Ctrl-C
+            quitEvent.WaitOne();
+
+            if (server != null)
+            {
+                Console.WriteLine("Server stopped. Waiting for exit...");
+
+                using (ReferenceServer _server = server)
+                {
+                    // Stop status thread
+                    server = null;
+                    status.Wait();
+                    // Stop server and dispose
+                    _server.Stop();
+                }
+            }
+
+            exitCode = ExitCode.Ok;
+        }
+
+        public static ExitCode ExitCode { get => exitCode; }
+
+        private static void CertificateValidator_CertificateValidation(CertificateValidator validator, CertificateValidationEventArgs e)
+        {
+            if (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted)
+            {
+                e.Accept = autoAccept;
+                if (autoAccept)
+                {
+                    Console.WriteLine("Accepted Certificate: {0}", e.Certificate.Subject);
+                }
+                else
+                {
+                    Console.WriteLine("Rejected Certificate: {0}", e.Certificate.Subject);
+                }
+            }
+        }
+
+        private async Task ConsoleSampleServer()
+        {
+            ApplicationInstance.MessageDlg = new ApplicationMessageDlg();
+            ApplicationInstance application = new ApplicationInstance();
+
+            application.ApplicationName = "Quickstart Reference Server";
+            application.ApplicationType = ApplicationType.Server;
+            application.ConfigSectionName = Utils.IsRunningOnMono() ? "Quickstarts.MonoReferenceServer" : "Quickstarts.ReferenceServer";
+
+            // load the application configuration.
+            ApplicationConfiguration config = await application.LoadApplicationConfiguration(false);
+
+            // check the application certificate.
+            bool haveAppCertificate = await application.CheckApplicationInstanceCertificate(false, CertificateFactory.defaultKeySize, CertificateFactory.defaultLifeTime);
+            if (!haveAppCertificate)
+            {
+                throw new Exception("Application instance certificate invalid!");
+            }
+
+            if (!config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+            {
+                config.CertificateValidator.CertificateValidation += new CertificateValidationEventHandler(CertificateValidator_CertificateValidation);
+            }
+
+            // start the server.
+            server = new ReferenceServer();
+            await application.Start(server);
+
+            // print endpoint info
+            var endpoints = application.Server.GetEndpoints().Select(e => e.EndpointUrl).Distinct();
+            foreach (var endpoint in endpoints)
+            {
+                Console.WriteLine(endpoint);
+            }
+
+            // start the status thread
+            status = Task.Run(new Action(StatusThread));
+
+            // print notification on session events
+            server.CurrentInstance.SessionManager.SessionActivated += EventStatus;
+            server.CurrentInstance.SessionManager.SessionClosing += EventStatus;
+            server.CurrentInstance.SessionManager.SessionCreated += EventStatus;
+        }
+
+        private void EventStatus(Session session, SessionEventReason reason)
+        {
+            lastEventTime = DateTime.UtcNow;
+            PrintSessionStatus(session, reason.ToString());
+        }
+
+        void PrintSessionStatus(Session session, string reason, bool lastContact = false)
+        {
+            lock (session.DiagnosticsLock)
+            {
+                string item = String.Format("{0,9}:{1,20}:", reason, session.SessionDiagnostics.SessionName);
+                if (lastContact)
+                {
+                    item += String.Format("Last Event:{0:HH:mm:ss}", session.SessionDiagnostics.ClientLastContactTime.ToLocalTime());
+                }
+                else
+                {
+                    if (session.Identity != null)
+                    {
+                        item += String.Format(":{0,20}", session.Identity.DisplayName);
+                    }
+                    item += String.Format(":{0}", session.Id);
+                }
+                Console.WriteLine(item);
+            }
+        }
+
+        private async void StatusThread()
+        {
+            while (server != null)
+            {
+                if (DateTime.UtcNow - lastEventTime > TimeSpan.FromMilliseconds(6000))
+                {
+                    IList<Session> sessions = server.CurrentInstance.SessionManager.GetSessions();
+                    for (int ii = 0; ii < sessions.Count; ii++)
+                    {
+                        Session session = sessions[ii];
+                        PrintSessionStatus(session, "-Status-", true);
+                    }
+                    lastEventTime = DateTime.UtcNow;
+                }
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
